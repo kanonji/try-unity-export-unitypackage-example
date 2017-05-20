219e7139d9dd04749680a95d091b489b75a81afd
kanonji <kanonji@users.noreply.github.com>
2017-05-20 22:45:30 +0900

    stash

--- ci/install.sh
+++ ci/install.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-BASE_URL=http://netstorage.unity3d.com/unity
+BASE_URL=http://download.unity3d.com/download_unity/
 
 download() {
   file=$1
@@ -12,9 +12,10 @@ download() {
 install() {
   package=$1
   download "${package}"
+ls -al
 
-  echo "Installing "`basename "$package"`
-  sudo installer -dumplog -package ./`basename "$package"` -target /
+  echo "Installing "`basename "${package}"`
+  sudo installer -dumplog -package `basename "${package}"` -target /
 }
 
 # See $BASE_URL/$UNITY_HASH/unity-$UNITY_VERSION-$PLATFORM.ini for complete list
