b9a46b3197d5b36c20b51896d96e0841800df904
kanonji <kanonji@users.noreply.github.com>
2017-05-21 00:36:54 +0900

    stash

--- ci/export.sh
+++ ci/export.sh
@@ -3,19 +3,12 @@ set -evu
 
 UNITY=/Applications/Unity/Unity.app/Contents/MacOS/Unity
 PROJECT_PATH=$(pwd)
-LOG_FILE="${PROJECT_PATH}/unity.log"
 
 ${UNITY} \
   -exportPackage ${EXPORT_DIRS} ${PACKAGE_NAME} \
   -projectPath $(pwd) \
   -batchmode \
   -nographics \
-  -logfile ${LOG_FILE} \
-  -quit &
-PID=$!
-tail -f ${LOG_FILE} &
-PID_TAIL=$!
-fg ${PID}
-kill ${PID_TAIL}
-cat ${LOG_FILE}
+  -quit \
+  -logfile &
 ls -al
